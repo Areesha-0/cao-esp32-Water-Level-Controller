a5d760fc23ebed80084fa2474baf435278f24e81
Areesha-0 <88435659+Areesha-0@users.noreply.github.com>
2022-04-09 18:15:06 +0500

    Update led_blink.ino

--- led_blink.ino
+++ led_blink.ino
@@ -1,10 +1,10 @@
-#define LED_BUILTIN 22
+#define LED_BUILTIN 22 //gpio pin 22 used for led
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH); //led on
-  delay(1000); //half a second
+  delay(1000); //a second
   digitalWrite(LED_BUILTIN, LOW); //led off
   delay(1000);
 }
