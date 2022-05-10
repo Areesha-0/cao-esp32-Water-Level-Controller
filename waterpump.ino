dc9f2df5006470603b5be3acbefc235b2ad39383
Areesha-0 <88435659+Areesha-0@users.noreply.github.com>
2022-05-11 00:14:18 +0500

    water pump working successful with blynk
    functionality of ultra sonic sensor added with the relay to turn the motor on and off

--- waterpump.ino
+++ waterpump.ino
@@ -1,29 +1,75 @@
-
 #define BLYNK_TEMPLATE_ID "TMPLg1OxXrwX"
 #define BLYNK_DEVICE_NAME "Water Pump Controller"
 #define BLYNK_AUTH_TOKEN "LhbCoP2ExeHueOYjSJqppGTrM0F0dAfo"
-
-
 #define BLYNK_PRINT Serial
-#include <WiFi.h> 
- 
+
+#include <WiFi.h>
+#include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
- 
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-char ssid[] = "muhammadasif7";  // Enter your wifi name
-char pass[] = "asif1972";  // Enter your wifi password
+// Your WiFi credentials.
+char ssid[] = "muhammadasif7";
+char pass[] = "asif1972";
 
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+
+long duration;
+long wave_distance; //in centimeter
+long tank_depth = 100;
+double water_level;
+const int trigPin = 33;
+const int echoPin = 32;
 const int relay = 13;
-void setup()
-{     
-  Serial.begin(115200);
-  Blynk.begin(auth, ssid, pass);    
-  pinMode(relay,OUTPUT);
+
+void ultrasonic_sensor(){
+   // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  wave_distance = duration * SOUND_SPEED/2; //distance (cm) from water level to the top of the tank
+  water_level = tank_depth - wave_distance;
+  water_level = (water_level / tank_depth) * 100 ; //calculating percentage of water present in the tank
+  
+  // Prints the distance from water level to top of tank and water level on the Serial Monitor
+  Serial.print("thank height left (cm): ");
+  Serial.println(wave_distance);
+  Serial.print("water level: ");
+  Serial.println(water_level);
+  
+  //turns relay off if the tank is about to be full automatically !but not the button on blynk!
+  if(water_level >= 95){
+    Serial.print("tank is full");
+    digitalWrite(relay, LOW);
+  }
+ 
+  Blynk.virtualWrite(V0, water_level);
+  delay(1000);
 }
 
-void loop()
-{
+//updates latest virtual pin value stored on server 
+BLYNK_CONNECTED(){
+  Blynk.syncVirtual(V0);
+}
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+  Blynk.begin(auth, ssid, pass);
+  pinMode(relay,OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+}
+
+void loop() {
   Blynk.run();
- }
+  ultrasonic_sensor();
+}
