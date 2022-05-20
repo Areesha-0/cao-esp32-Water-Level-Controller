04bcaf01fff736cffca5b891f4647b22b952fb7e
Areesha-0 <88435659+Areesha-0@users.noreply.github.com>
2022-05-21 03:33:26 +0500

    storing state of motor and tank depth on nvs
    using preferences.h to store the state of relay acting as switch for the motor and the tank depth

--- Web Server: Water Pump Controller/Using Spiffs/relay+sensor.ino
+++ Web Server: Water Pump Controller/Using Spiffs/relay+sensor.ino
@@ -4,22 +4,28 @@
 #include "SPIFFS.h"
 #include <Wire.h>
 #include <string.h>
+#include <Preferences.h>
 
 //network credentials
 const char* ssid = "muhammadasif7";
 const char* password = "asif1972";
 
-const int relay_pin = 13;
 // Stores relay state
 String relay_state;
 const char* PARAM_INPUT_1 = "input1"; //getting tank depth
 const char* PARAM_INPUT_2 = "input2"; //getting level until which the tank should be filled
+
 const int trig_pin = 33;
 const int echo_pin = 32;
+const int relay_pin = 13;
+
 float tank_depth = 0;
 float water_level = 0; //calculated using the ultrasonic sensor
 float desired_waterlevel = 0; //given by user
 
+bool motor_state; //saves to nvs
+
+Preferences preferences;
 Ultrasonic ultrasonic_sensor(trig_pin, echo_pin);
 
 // Create AsyncWebServer object on port 80
@@ -45,6 +51,9 @@ String processor(const String& var){
   else if(var == "WATERLEVEL"){
     return display_gauge();
   }
+    else if(var == "TANKDEPTH"){
+    return display_depth();
+  }
   return String();
 }
 
@@ -53,27 +62,34 @@ String get_waterlevel(){
   float distance_waterToTankTop = ultrasonic_sensor.read();
   water_level = tank_depth - distance_waterToTankTop ;
   water_level = (water_level / tank_depth) * 100;
+
+  Serial.println("depth: ");
+  Serial.println(tank_depth);
   
   if(isnan(distance_waterToTankTop)){
      Serial.println("Did not get any reading from the ultrasonic sensor");
      return "";   
   }
   else{ 
-    if(water_level == desired_waterlevel){ //water is about to reach the limit set
+    if(water_level >= desired_waterlevel){ //water is about to reach the limit set
       digitalWrite(relay_pin, LOW);
       Serial.println("motor turned off at desired water level");
+      preferences.putBool("state", 0);
+      Serial.printf("State saved: 0");
     }
     
     if(water_level >= 90){ //water is about to overflow then turn off the motor
       digitalWrite(relay_pin, LOW);
       Serial.println("motor was about to overflow");
+      preferences.putBool("state", 0);
+      Serial.printf("State saved: 0");
     }
 
      Serial.println("desired water level (cm): ");
      Serial.println(desired_waterlevel); //for the chart
      Serial.println("water level (cm): ");
      Serial.println(distance_waterToTankTop); //for the chart
-     Serial.println("water level %: ");
+     Serial.println("water level percentage: ");
      Serial.println(water_level); //for the chart
      
      return String(distance_waterToTankTop);
@@ -86,19 +102,38 @@ String display_gauge(){
       return "";
   }
   else {
-      Serial.println("water level %: ");
-      Serial.println(water_level); //for the chart
       return String(water_level);
   } 
 }
 
+String display_depth(){
+  if(isnan(tank_depth)){
+      Serial.println("tank depth not detected!");
+      return "";
+  }
+  else {
+      return String(tank_depth);
+  } 
+}
+
+void print_details(){
+  
+}
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
 
+  preferences.begin("tank_depth", false);
+  tank_depth = preferences.getFloat("depth",0); //returns default value 0 if it does not have depth 
+  
+ //Create a namespace called "motor_state"
+  preferences.begin("motor_state", false); //read write mode
+  motor_state = preferences.getBool("state", false);   // read the last motor state from flash memory
+
   //relay is in NC configuration
   pinMode(relay_pin, OUTPUT);
-  digitalWrite(relay_pin, LOW);
+  digitalWrite(relay_pin, motor_state);
+
 
   // Initialize SPIFFS
   if (! SPIFFS.begin ()) {
@@ -130,13 +165,22 @@ void setup(){
   
   // Route to set relay to HIGH
   server.on("/motor_on", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(relay_pin, HIGH);    
+    digitalWrite(relay_pin, HIGH); 
+       
+    // save motor's state in flash memory
+    preferences.putBool("state", 1);
+    Serial.printf("State saved: 1");
+    
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
   
   // Route to set GPIO to LOW
   server.on("/motor_off", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(relay_pin, LOW);    
+    // save the LED state in flash memory
+    preferences.putBool("state", 0);
+    Serial.printf("State saved: 0");
+    
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
 
@@ -152,19 +196,23 @@ void setup(){
     if (request->hasParam(PARAM_INPUT_1)) {
       inputMessage = request->getParam(PARAM_INPUT_1)->value();
       inputParam = PARAM_INPUT_1;
+      
       Serial.println("depth of the tank is: ");
       Serial.println(inputMessage);
+      
       tank_depth = inputMessage.toFloat(); //setting tank depth for program
-      Serial.println(tank_depth);
+      
+      preferences.putFloat("depth", tank_depth); //saving depth given by the user
     }
 
     else if (request->hasParam(PARAM_INPUT_2)) {
       inputMessage = request->getParam(PARAM_INPUT_2)->value();
       inputParam = PARAM_INPUT_2;
+      
       Serial.println("Turn of the motor at water level (%): ");
       Serial.println(inputMessage);
+      
       desired_waterlevel = inputMessage.toFloat(); //saving water level
-      Serial.println(desired_waterlevel);
     }
     
     else {
@@ -175,7 +223,6 @@ void setup(){
                                      + inputParam + ") with value: " + inputMessage +
                                      "<br><a href=\"/\">Return to Home Page</a>");
   });
-  
   // Start server
   server.onNotFound(notFound);
   server.begin();
